adc129d3ca553dfbcac07cbf698b544d62b5dd3b
RickCodeworks <rjedgemon@hotmail.com>
2020-04-23 19:19:38 -0600

    delete edit icons

--- clientsrc/src/components/Comment.vue
+++ clientsrc/src/components/Comment.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="comment bg-light w-75 m-auto rounded shadow">Comment here...
     <p>{{commentData.body}}</p>
-
+    <button type="submit">
+      <i class="fas fa-pencil-alt text-warning"></i>
+    </button>
+    <button class="btn btn-sm" @click="deleteComment()">
+      <i class="fas fa-trash-alt text-danger"></i>
+    </button>
   </div>
 </template>
 
 
 <script>
-export default {
-  name: "comment",
-  data() {
-    return {};
-  },
-  computed: {},
-  methods: {
-    deleteComment() {
-      this.$store.dispatch("deleteComment", this.commentData);
-      this.$store.dispatch("getComments", this.comment.taskId);
-    }
-  },
-  components: {}
-};
+  export default {
+    name: "comment",
+    data() {
+      return {};
+    },
+    computed: {},
+    methods: {
+      deleteComment() {
+        this.$store.dispatch("deleteComment", this.commentData);
+        this.$store.dispatch("getComments", this.comment.taskId);
+      }
+    },
+    components: {}
+  };
 </script>
 
 
